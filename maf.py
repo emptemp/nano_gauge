7f125d442a6e6f9385cdf8d53a612a0c606f5ef6
emptemp <gerald.tschinkl@gmail.com>
2018-03-17 19:50:46 +0100

    new filter

--- maf.py
+++ maf.py
@@ -1,23 +1,43 @@
+from __future__ import division
 import matplotlib.pyplot as plt
 
-fbuflen = 420
-i = 0
-
-fbuf = [0] * fbuflen
 uftd = []
 ftd = []
 
-with open("log.txt", 'r') as f:
-  lines = f.readlines()
-  for line in lines:
-    fbuf[i%fbuflen] = int(line)
+def t2f(inp):
+  fq = []
+  for val in inp:
+    fq.append(1/(val/1000000))
+  return fq
+
+def mafilter(inp):
+  fbuflen = 210
+  fbuf = [0] * fbuflen
+  ftdbuf = [] 
+  i = 0
+  for val in inp:  
+    fbuf[i%fbuflen] = int(val)
     i += 1
     average = sum(fbuf)/fbuflen
-    ftd.append(average)
-    uftd.append(int(line))
+    ftdbuf.append(average)
+  return ftdbuf
 
-#print ftd
+with open("log_ftd.txt", 'r') as f:
+  lines = f.readlines()
+  for line in lines:
+    if len(line.split(",")) == 2:
+      uftd.append(int(line.split(",")[0]))
+      ftd.append(int(line.split(",")[1]))
 
-plt.plot(ftd)
-plt.plot(uftd)
+#print uftd
+#print ftd 
+
+#plt.plot(mafilter(uftd), label='py_ftd')
+#plt.plot(uftd, label='uftd')
+#plt.plot(ftd, label='ard_ftd')
+
+plt.plot(t2f(uftd), label='uftd_fq')
+plt.plot(t2f(ftd), label='ard_ftd_fq')
+
+plt.legend()
 plt.show()
--- nano_gauge/nano_gauge.ino
+++ nano_gauge/nano_gauge.ino
@@ -6,28 +6,26 @@ const byte motorPin3 = 10;
 const byte motorPin4 = 11;
 const byte inputPin = 7;
 
-int fss = 160;
-int delayTime = 4255;
+const uint8_t fss = 160;
+uint16_t delayTime = 4255;
 
-unsigned int a;
-unsigned int b;
-unsigned int c;
-unsigned int d;
+uint8_t a;
+uint8_t b;
+uint8_t c;
+uint8_t d;
 
 bool  ccw=0;
 float e=2.71;
-int   current=0;
-int   pos = 0;
+uint16_t current=0;
+uint16_t pos = 0;
 
-volatile unsigned long current_time = 0;
-volatile long previous_time = 0;
-volatile unsigned long duration;
+volatile uint32_t current_time = 0;
+volatile uint32_t previous_time = 0;
+volatile uint32_t duration;
 
-const uint8_t fbuflen = 420;
+const uint16_t fbuflen = 420;
 uint16_t fbuf[fbuflen];
 uint8_t i = 0;
-uint32_t average = 0;
-uint16_t rpm = 0;
 
 void setup() { 
   Serial.begin(115200);
@@ -45,22 +43,20 @@ void setup() {
 #endif
 }
 
-void turn(int fullsteps, bool ccw)
+void turn(int16_t fullsteps, bool ccw)
 {
   /// 1 schleifendurchgang entspricht 4 pi rad
-  
-  /// fz ist fss
-  int fz = fss;
+  uint16_t fz = fss;
   
   /// muss degree/2 mal ausgeführt werden für degree
-  for(int n=0; n<fullsteps; n++)
+  for(int16_t n=0; n<fullsteps; n++)
   {
     //Serial.print("n: ");
     //Serial.print(n); 
     //Serial.print("\n");
     delayTime = 1000000/(3*fz);
     ///   jede schleife entspricht 2 degree (2° / 4 pi)
-    for(int k=5; k>=0; k--)
+    for(int8_t k=5; k>=0; k--)
     {               
       a = 1 & (0x38 >> abs(5*ccw - k)); ///00111000 >> k 
       b = 1 & (0x0E >> abs(5*ccw - k)); ///00011100 >> k
@@ -76,12 +72,12 @@ void turn(int fullsteps, bool ccw)
     if(n < fullsteps/2)         { fz = fz + e*fz/pow(n+2,2);  }
     else if(n >= fullsteps/2)   { fz = fz - e*fz/pow(fullsteps-n+2,2);  }
 
-    Serial.print(fz);
-    Serial.print("\n");
+    //Serial.print(fz);
+    //Serial.print("\n");
   }
 }
 
-void position(uint16_t future)
+void position(int16_t future)
 {
    future = future - current;
    current = current + future;
@@ -90,22 +86,24 @@ void position(uint16_t future)
    else                 {  }  
 }
 
-
-void loop() 
+uint16_t average()
 {
-  Serial.print(duration);
-  Serial.print(",");
-  
-  fbuf[i%fbuflen] = duration;
-  i++;
-  for(int k = 0; k < fbuflen; k++)
+  uint32_t average = 0;
+  for(uint16_t k = 0; k < fbuflen; k++)
   {
     average += fbuf[k];
   }
-  average = average/fbuflen;
-  Serial.print(average);
+  return average/fbuflen;
+}
+
+void loop() 
+{
+  noInterrupts();
+  Serial.print(duration);
+  Serial.print(",");
+  Serial.print(average());
   Serial.print("\n");
-  average = 0;  
+  interrupts();
 }
 
 ISR(ANALOG_COMP_vect)
@@ -113,4 +111,5 @@ ISR(ANALOG_COMP_vect)
    current_time = micros();
    duration = current_time - previous_time;
    previous_time = current_time;
+   fbuf[i%fbuflen] = duration; i++;
 }
